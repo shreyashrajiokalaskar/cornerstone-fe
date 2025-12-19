700a684c23c2f1a747c5be94f9d18d36ab1798de
Shreyash Kalaskar <shreyashrajiokalaskar@gmail.com>
2025-12-19 11:24:42 +0530

    Added shared types lib

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,9 @@
+import { IUser } from "@cornerstone/types";
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
+  getData(user?: IUser): { message: string } {
     return { message: 'Hello API' };
   }
 }
